5bdf5e58a381907ea9f7df49d26c6f442dc5812c
Sadorbt <131970636+Sadorbt@users.noreply.github.com>
2023-04-27 05:27:55 -0700

    Update and rename hello9.py to Next_20_leap_years.py

--- Next_20_leap_years.py
+++ Next_20_leap_years.py
@@ -15,11 +15,4 @@ def print_next_leap_years(next_leap_year, no_of_leap_years):
             no_of_leap_years_found += 1
             print(next_leap_year)
         next_leap_year += 1
-# def print_next_leap_years_repeatly(next_leap_year, no_of_leap_years, no_of_leap_years_found = 0):
-#     if is_leap_year(next_leap_year):
-#         no_of_leap_years_found += 1
-#         print(next_leap_year)
-#     if no_of_leap_years_found < no_of_leap_years:
-#         print_next_leap_years_repeatly(next_leap_year + 1, no_of_leap_years, no_of_leap_years_found)
 print_next_leap_years(next_leap_year, no_of_leap_years)
-# print_next_leap_years_repeatly(next_leap_year, no_of_leap_years)
