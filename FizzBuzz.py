8c485cf624936febcdc1417e62f6cab06dd55b18
Sadorbt <131970636+Sadorbt@users.noreply.github.com>
2023-05-03 08:40:26 -0700

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,17 +2,14 @@ n_number = int (input ("Input a number here: "))
 n = range(1, n_number+1)
 
 def my_function(n):
-    output = []
-    print (output)
     for i in n :
-        if (i%3 == 0 or i%5 == 0) :
-            output.append(" FizzBuzz ")
+        if (i%3 == 0 or i%5 == 0):
+            print(" FizzBuzz ")
         elif i%3==0:
-            output.append(" Fizz ")
+            print(" Fizz ")
         elif i%5==0:
-            output.append(" Buzz ")
+            print(" Buzz ")
         else:
-            output.append(i)
-    print (output)
+            print(i)
     
 my_function(n)
