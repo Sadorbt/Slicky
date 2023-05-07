2b556a39e952b0ee70e12855329791f0c443e360
Sadorbt <131970636+Sadorbt@users.noreply.github.com>
2023-05-06 21:49:41 -0700

    Update Calculate_Pay_Via_Comission_Of_Sales.py

--- Calculate_Pay_Via_Comission_Of_Sales.py
+++ Calculate_Pay_Via_Comission_Of_Sales.py
@@ -1,7 +1,7 @@
 sales = int(input("What's your total sales for the month? "))
 amount = int(input("How much advance have you taken this month? "))
 advance = amount - 2000
-def calculatepay(sales, advance):
+def calculatepay():
  if sales<=10000:
   salary = (sales * 0.1) - advance
  elif sales>10000 and sales<=14999:
@@ -16,4 +16,4 @@ def calculatepay(sales, advance):
    print("Please reimburse the company")
   else:
    print (salary)
-
+ calculatepay()
